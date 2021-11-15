e636d15067e5921fd1c0f0b6ef4b17221865ca67
Shahrizoda <87145658+Shahrizoda888@users.noreply.github.com>
2021-11-15 13:21:39 +0500

    blog-post

--- blog/urls.py
+++ blog/urls.py
@@ -8,11 +8,11 @@ from .views import (
     BlogDeleteView
 )
 urlpatterns = [
-    path('post/<int:pk>/delete/',BlogDeleteView.as_view(),name='post_delete'),
-    path('post/<int:pk>/edit/',BlogUpdateView.as_view(),name='post_edit'),
-    path('',BlogListView.as_view(),name='home'),
-    path('post/<int:pk>/',BlogDetailView.as_view(),name='post_detail'),
-    path('post/new/',BlogCreateView.as_view(), name='post_new')
+    path('post/<int:pk>/delete/',BlogDeleteView,name='post_delete'),
+    path('post/<int:pk>/edit/',BlogUpdateView,name='post_edit'),
+    path('',BlogListView,name='home'),
+    path('post/<int:pk>/',BlogDetailView,name='post_detail'),
+    path('post/new/',BlogCreateView, name='post_new')
 
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from .models import Post
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 # Create your views here.
 class BlogListView(ListView):
@@ -14,19 +16,19 @@ class BlogDetailView(DetailView):
     model=Post
     template_name='post_detail.html'
 
-
+@login_required(login_url='login')
 class BlogCreateView(CreateView):
     model=Post
     template_name='post_new.html'
     fields=['title','author','body']
 
-
+@login_required(login_url='login')
 class BlogUpdateView(UpdateView):
     model=Post
     template_name='post_edit.html'
     fields=['title','body']
 
-
+@login_required(login_url='login')
 class BlogDeleteView(DeleteView):
     model=Post
     template_name='post_delete.html'
